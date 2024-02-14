52feae372ff993500c22130a01119587eca0d2b3
Jayden <jecathcart@uncg.edu>
2024-02-13 21:42:14 -0500

    final

---
+++ csc300x-hw3/script/script.js
@@ -0,0 +1,15 @@
+const gallery = document.querySelectorAll('img');
+for (let index = 0; index < gallery.length; index++) {
+    const element = gallery[index];
+    element.addEventListener('click', expand);
+}
+function expand(event) {
+    const smallImage = event.currentTarget;
+    const bigImage = document.querySelector(".big");
+    bigImage.classList.remove('big');
+    bigImage.classList.add('small');
+    smallImage.classList.remove('small');
+    smallImage.classList.add('big');
+}
+
+
